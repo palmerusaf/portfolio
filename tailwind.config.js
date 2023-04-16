2451228bb2bafe2fd0486f2c0af7a5cff2eb5249
Branden Palmer <brandenpalmer08@gmail.com>
2023-04-16 17:18:25 +0700

    add hacker plugin to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+const plugin = require("tailwindcss/plugin");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {},
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    plugin(function({ addVariant }) {
+      // here is your CSS selector - could be anything
+      // in this case it is `.theme` element
+      // with `.theme--red` class (both present)
+      addVariant("hacker", "html.hacker &");
+    }),
+  ],
 };
