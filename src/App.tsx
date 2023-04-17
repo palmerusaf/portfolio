e3e3e3230ed2b0e8893f073f7f7ca63f66a2baec
Branden Palmer <brandenpalmer08@gmail.com>
2023-04-17 19:51:45 +0700

    add active class to nav button when section in view

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,45 @@
+import { useRef } from 'react';
 import { NavBar } from './components/NavBar';
 
 function App() {
+  const homeRef: any = useRef<HTMLDivElement>();
+  const aboutRef: any = useRef<HTMLDivElement>();
+  const skillsRef: any = useRef<HTMLDivElement>();
+  const projectsRef: any = useRef<HTMLDivElement>();
+  const connectRef: any = useRef<HTMLDivElement>();
   return (
     <div className=''>
-      <NavBar />
+      <NavBar refs={[homeRef, aboutRef, skillsRef, projectsRef, connectRef]} />
       <div
+        ref={homeRef}
         id='home'
         className='flex justify-center items-center h-screen text-3xl'
       >
         Home
       </div>
       <div
+        ref={aboutRef}
         id='about'
         className='flex justify-center items-center h-screen text-3xl'
       >
         about
       </div>
       <div
+        ref={skillsRef}
         id='skills'
         className='flex justify-center items-center h-screen text-3xl'
       >
         skills
       </div>
       <div
+        ref={projectsRef}
         id='projects'
         className='flex justify-center items-center h-screen text-3xl'
       >
         projects
       </div>
       <div
+        ref={connectRef}
         id='connect'
         className='flex justify-center items-center h-screen text-3xl'
       >
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,8 +1,8 @@
 import { Cross as Ham } from 'hamburger-react';
-import { useState } from 'react';
+import { MutableRefObject, useEffect, useState } from 'react';
 import { ThemeToggle } from '../ThemeToggle';
 
-export const NavBar = () => {
+export const NavBar = ({ refs }: { refs: any[] }) => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <nav className='flex sticky top-0 justify-between items-center p-2 w-full bg-white duration-300 md:p-6 dark:text-white dark:bg-gray-700 hacker:bg-black hacker:text-green-500'>
@@ -11,22 +11,26 @@ export const NavBar = () => {
         className={`fixed right-0 top-0 flex h-full flex-col justify-center gap-4 bg-white px-8 text-center text-xl capitalize duration-300 hacker:bg-black hacker:text-green-500 dark:bg-gray-700 dark:text-white md:static md:ml-auto md:translate-x-0 md:flex-row ${isOpen ? 'translate-x-0' : 'translate-x-full'
           }`}
       >
-        {['home', 'about', 'skills', 'projects', 'connect'].map((item) => {
-          return (
-            <li>
-              <button
-                onClick={() => {
-                  setIsOpen(false);
-                  document
-                    .getElementById(item)
-                    ?.scrollIntoView({ behavior: 'smooth' });
-                }}
-              >
-                {item}
-              </button>
-            </li>
-          );
-        })}
+        {['home', 'about', 'skills', 'projects', 'connect'].map(
+          (item, index) => {
+            const onScreen = useOnScreen(refs[index]);
+            return (
+              <li>
+                <button
+                  className={`${onScreen ? 'underline' : ''}`}
+                  onClick={() => {
+                    setIsOpen(false);
+                    document
+                      .getElementById(item)
+                      ?.scrollIntoView({ behavior: 'smooth' });
+                  }}
+                >
+                  {item}
+                </button>
+              </li>
+            );
+          }
+        )}
       </ul>
       <div className='flex items-center'>
         <span className='md:hidden'>
@@ -37,3 +41,29 @@ export const NavBar = () => {
     </nav>
   );
 };
+
+// Hook
+function useOnScreen<T extends Element>(ref: MutableRefObject<T>): boolean {
+  // State and setter for storing whether element is visible
+  const [isIntersecting, setIntersecting] = useState<boolean>(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        // Update our state when observer callback fires
+        setIntersecting(entry.isIntersecting);
+      },
+      {
+        threshold: 0.75,
+      }
+    );
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+    return () => {
+      observer.unobserve(ref.current);
+    };
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+
+  return isIntersecting;
+}
