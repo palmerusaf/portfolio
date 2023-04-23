ac616f1083532880b5af281b1815fb1489b77529
Branden Palmer <brandenpalmer08@gmail.com>
2023-04-23 14:40:34 +0700

    fix gap between nav and background on mobile

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -2,7 +2,7 @@ import DarkBg from './dark-sm.jpg';
 import DarkBgMd from './dark.jpg';
 import LightBg from './light.jpg';
 export const Background = () => (
-  <div className='fixed left-0 top-16 w-screen h-screen md:top-20 -z-10'>
+  <div className='fixed left-0 top-14 w-screen h-screen md:top-20 -z-10'>
     <div
       className='absolute top-0 left-0 w-full h-full duration-300 dark:opacity-0 hacker:opacity-0'
       style={{ backgroundImage: `url(${LightBg})` }}
