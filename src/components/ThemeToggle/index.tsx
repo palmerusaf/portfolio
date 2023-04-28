33f211e197729b932ed5b919c4a4d27e5dd6a5ba
Branden Palmer <brandenpalmer08@gmail.com>
2023-04-28 15:56:41 +0700

    fix theme toggle bug where state reset on scroll

--- src/components/ThemeToggle/index.tsx
+++ src/components/ThemeToggle/index.tsx
@@ -1,21 +1,22 @@
+import { useRef } from 'react';
 import Hoodie from './hoodie';
 import Moon from './moon';
 import Sun from './sun';
 
 export function ThemeToggle() {
-  let indexRef = 0;
+  let indexRef = useRef(0);
   const themes = ['', 'dark', 'hacker'];
   const cycleTheme = () =>
-    (document.body.className = themes[++indexRef % themes.length]);
+    (document.body.className = themes[++indexRef.current % themes.length]);
   return (
     <button
       onClick={cycleTheme}
-      className='flex relative gap-1 justify-center w-20 h-6 bg-yellow-300 rounded-full border-2 border-pink-500 duration-300 dark:bg-black dark:border-purple-500 hacker:border-green-500 hacker:bg-black'
+      className='relative flex h-6 w-20 justify-center gap-1 rounded-full border-2 border-pink-500 bg-yellow-300 duration-300 hacker:border-green-500 hacker:bg-black dark:border-purple-500 dark:bg-black'
     >
-      <span className='absolute z-50 p-3 bg-pink-500 rounded-full duration-300 -translate-x-full dark:bg-purple-500 dark:translate-x-0 -translate-y-[2px] hacker:translate-x-full hacker:bg-green-500'></span>
-      <Sun className='h-full duration-300 fill-pink-500 hacker:fill-green-500 dark:fill-purple-500' />
-      <Moon className='h-full duration-300 fill-pink-500 hacker:fill-green-500 dark:fill-purple-500' />
-      <Hoodie className='h-full duration-300 fill-pink-500 hacker:fill-green-500 dark:fill-purple-500' />
+      <span className='absolute z-50 -translate-x-full -translate-y-[2px] rounded-full bg-pink-500 p-3 duration-300 hacker:translate-x-full hacker:bg-green-500 dark:translate-x-0 dark:bg-purple-500'></span>
+      <Sun className='h-full fill-pink-500 duration-300 hacker:fill-green-500 dark:fill-purple-500' />
+      <Moon className='h-full fill-pink-500 duration-300 hacker:fill-green-500 dark:fill-purple-500' />
+      <Hoodie className='h-full fill-pink-500 duration-300 hacker:fill-green-500 dark:fill-purple-500' />
     </button>
   );
 }
