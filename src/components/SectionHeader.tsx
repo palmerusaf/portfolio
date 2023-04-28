cf5cc00d1441a6f6b9b60844a80a32e1216f95bf
Branden Palmer <brandenpalmer08@gmail.com>
2023-04-28 17:28:38 +0700

    fix sidescroll on chrome

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -1,8 +1,8 @@
 export const SectionHeader = ({ label }: { label: string }) => (
-  <div className='flex px-5 w-screen'>
-    <div className='flex items-center p-2 my-4 mx-auto w-full max-w-xl bg-yellow-300 duration-300 -skew-x-12 shadow-[10px_10px] shadow-pink-500 hacker:skew-x-0 hacker:rounded-sm hacker:border hacker:border-green-500 hacker:bg-black hacker:shadow hacker:shadow-green-500'>
+  <div className='flex w-full px-5'>
+    <div className='mx-auto my-4 flex w-full max-w-xl -skew-x-12 items-center bg-yellow-300 p-2 shadow-[10px_10px] shadow-pink-500 duration-300 hacker:skew-x-0 hacker:rounded-sm hacker:border hacker:border-green-500 hacker:bg-black hacker:shadow hacker:shadow-green-500'>
       <hr className='w-full border border-blue-500 duration-300 hacker:border-green-500' />
-      <h2 className='mx-2 text-4xl text-pink-500 duration-300 font-thunder hacker:font-glitch hacker:text-green-500'>
+      <h2 className='mx-2 font-thunder text-4xl text-pink-500 duration-300 hacker:font-glitch hacker:text-green-500'>
         {label}
       </h2>
       <hr className='w-full border border-blue-500 duration-300 hacker:border-green-500' />
