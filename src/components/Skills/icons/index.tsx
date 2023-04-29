e82b061340ce49fb7e56420e34b470c199c65318
Branden Palmer <brandenpalmer08@gmail.com>
2023-04-29 14:16:06 +0700

    add skill icons

---
+++ src/components/Skills/icons/index.tsx
@@ -0,0 +1,25 @@
+import FirebaseIcon from './firebase.svg';
+import GitIcon from './git.svg';
+import JestIcon from './jest.svg';
+import JSIcon from './js.svg';
+import NextIcon from './next.svg';
+import NPMIcon from './npm.svg';
+import ReactIcon from './react.svg';
+import SassIcon from './sass.svg';
+import TailwindIcon from './tailwind.svg';
+import TSIcon from './ts.svg';
+import WebpackIcon from './webpack.svg';
+
+export {
+  JSIcon,
+  TSIcon,
+  GitIcon,
+  NPMIcon,
+  JestIcon,
+  NextIcon,
+  ReactIcon,
+  SassIcon,
+  WebpackIcon,
+  TailwindIcon,
+  FirebaseIcon,
+};
