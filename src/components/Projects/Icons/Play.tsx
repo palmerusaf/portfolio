bf4e8a91bf7563361c8bc99f65bc78a4291a2d94
Branden Palmer <brandenpalmer08@gmail.com>
2023-05-01 20:56:27 +0700

    add svgs for preview and code

---
+++ src/components/Projects/Icons/Play.tsx
@@ -0,0 +1,11 @@
+export const PlaySvg = ({ className }: { className: string }) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    className={className}
+    width='24'
+    height='24'
+    viewBox='0 96 960 960'
+  >
+    <path d='M340.001 817.613V329.926l383.073 243.843-383.073 243.844Z' />
+  </svg>
+);
--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,3 +1,5 @@
+import { CodeSvg } from './Icons/Code';
+import { PlaySvg } from './Icons/Play';
 import { useIndexer } from './IndexHook';
 
 export const Projects = () => {
@@ -7,8 +9,35 @@ export const Projects = () => {
   return (
     <div className='mx-2 mt-3 md:mx-auto md:mt-6 md:max-w-2xl'>
       {array[index]}
+      <div className='grid grid-cols-2 gap-4'>
+        <Link label='demo' svg={PlaySvg} />
+        <Link label='code' svg={CodeSvg} />
+      </div>
       <button onClick={nextIndex}>next</button>
       <button onClick={prevIndex}>prev</button>
     </div>
   );
 };
+
+interface LinkProps {
+  label: string;
+  svg: ({ className }: { className: string }) => JSX.Element;
+  href?: string;
+}
+
+function Link({ label, svg, href }: LinkProps) {
+  return (
+    <a
+      rel='noopener'
+      target='_blank'
+      href={href}
+      className='flex gap-2 justify-evenly items-center py-1 px-2 text-xl font-bold text-center text-pink-500 bg-yellow-300 shadow duration-300 skew-x-12 cursor-pointer dark:tracking-widest dark:-skew-x-12 shadow-gray-500 hacker:skew-x-0 hacker:border hacker:border-green-500 hacker:bg-black hacker:tracking-normal hacker:text-green-500 hacker:shadow-green-500 dark:bg-slate-900 dark:shadow-purple-400'
+    >
+      {svg({
+        className:
+          'w-8 h-full duration-300 fill-pink-500 hacker:fill-green-500 ',
+      })}
+      {label}
+    </a>
+  );
+}
