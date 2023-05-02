15f74aa43eceb04b3b5fc8e8d94cf6b63ea46a94
Branden Palmer <brandenpalmer08@gmail.com>
2023-05-02 16:21:11 +0700

    make about dark skew same direction

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ function App() {
       <Section pRef={aboutRef}>
         <SectionHeader label='About' />
         <div className='flex px-4 mt-2'>
-          <div className='flex overflow-hidden flex-col p-4 mx-auto max-w-xl font-bold bg-emerald-200 shadow-lg duration-300 skew-x-3 md:text-lg dark:tracking-widest dark:text-purple-400 indent-6 shadow-gray-500 hacker:skew-x-0 hacker:bg-black hacker:text-sm hacker:tracking-normal hacker:text-green-500 hacker:shadow-green-500 md:hacker:text-base dark:bg-slate-900 dark:shadow-purple-400'>
+          <div className='flex overflow-hidden flex-col p-4 mx-auto max-w-xl font-bold bg-emerald-200 shadow-lg duration-300 skew-x-3 md:text-lg dark:tracking-widest dark:text-purple-400 dark:-skew-x-3 indent-6 shadow-gray-500 hacker:skew-x-0 hacker:bg-black hacker:text-sm hacker:tracking-normal hacker:text-green-500 hacker:shadow-green-500 md:hacker:text-base dark:bg-slate-900 dark:shadow-purple-400'>
             <p>
               Hello there, I'm Branden. I'm a front-end developer/Air Force
               veteran from the USA. Honestly though, I'm just a big nerd.
