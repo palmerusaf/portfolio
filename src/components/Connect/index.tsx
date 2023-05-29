678b40d78974370f708b7b473ddc637367f43a8e
Branden Palmer <brandenpalmer08@gmail.com>
2023-05-29 22:07:53 +0700

    add blog link

--- src/components/Connect/index.tsx
+++ src/components/Connect/index.tsx
@@ -1,10 +1,12 @@
+import { BlogSvg } from './BlogSvg';
 import { EmailSvg } from './EmailSvg';
 import { GithubSvg } from './GithubSvg';
 import { LinkedInSvg } from './LinkedInSvg';
 import { ResumeSvg } from './ResumeSvg';
 
 export const Connect = () => (
-  <ul className='flex flex-col gap-10 items-center mt-36 w-full'>
+  <ul className='mt-28 flex w-full flex-col items-center gap-10'>
+    <Link label='blog' svg={BlogSvg} href='https://blog.brandenpalmer.dev' />
     <Link
       href='mailto:brandenpalmer08@gmail.com'
       svg={EmailSvg}
@@ -37,7 +39,7 @@ function Link({ label, svg, href }: LinkProps) {
         rel='noopener'
         target='_blank'
         href={href}
-        className='flex gap-2 items-center py-1 px-3 text-3xl font-bold bg-emerald-200 shadow duration-300 skew-x-3 cursor-pointer dark:tracking-widest dark:text-purple-400 hover:py-1 hover:px-2 hover:text-pink-500 hover:bg-yellow-300 hover:shadow hover:skew-x-12 shadow-gray-500 hacker:skew-x-0 hacker:bg-black hacker:tracking-normal hacker:text-green-500 hacker:shadow-green-500 dark:bg-slate-900 dark:shadow-purple-400 hover:hacker:skew-x-0 hover:hacker:border hover:hacker:border-green-500 hover:hacker:shadow-green-500 hover:dark:-skew-x-12 hover:dark:shadow-purple-400'
+        className='flex skew-x-3 cursor-pointer items-center gap-2 bg-emerald-200 px-3 py-1 text-3xl font-bold shadow shadow-gray-500 duration-300 hover:skew-x-12 hover:bg-yellow-300 hover:px-2 hover:py-1 hover:text-pink-500 hover:shadow hacker:skew-x-0 hacker:bg-black hacker:tracking-normal hacker:text-green-500 hacker:shadow-green-500 hover:hacker:skew-x-0 hover:hacker:border hover:hacker:border-green-500 hover:hacker:shadow-green-500 dark:bg-slate-900 dark:tracking-widest dark:text-purple-400 dark:shadow-purple-400 hover:dark:-skew-x-12 hover:dark:shadow-purple-400'
       >
         {svg({
           className:
