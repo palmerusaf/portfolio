0baef1a67d65da61d4e169c209b0ebc589b2f4a1
Branden Palmer <brandenpalmer08@gmail.com>
2023-11-01 22:38:34 -0500

    adj onscreen hook

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -20,8 +20,8 @@ export const NavBar = ({ refs }: { refs: any[] }) => {
               <li key={index}>
                 <button
                   className={`duration-300 hacker:text-sm dark:font-galaxy dark:tracking-wider ${onScreen
-                    ? 'skew-x-12 bg-yellow-300 px-2 py-1 text-pink-500 shadow hacker:skew-x-0 hacker:border hacker:border-green-500 hacker:bg-inherit hacker:text-inherit hacker:shadow-green-500 dark:-skew-x-12 dark:bg-inherit dark:shadow-purple-500'
-                    : ''
+                      ? 'skew-x-12 bg-yellow-300 px-2 py-1 text-pink-500 shadow hacker:skew-x-0 hacker:border hacker:border-green-500 hacker:bg-inherit hacker:text-inherit hacker:shadow-green-500 dark:-skew-x-12 dark:bg-inherit dark:shadow-purple-500'
+                      : ''
                     }`}
                   onClick={() => {
                     setIsOpen(false);
@@ -57,7 +57,7 @@ function useOnScreen<T extends Element>(ref: MutableRefObject<T>): boolean {
         setIntersecting(entry.isIntersecting);
       },
       {
-        threshold: 0.75,
+        threshold: 0.7,
       }
     );
     if (ref.current) {
